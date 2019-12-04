ad55faf21792213b03f0f755c00535917b088c0a
wangbinzero <wangbbhtt@gmail.com>
2019-12-04 18:09:20 +0800

    erc20转账

--- erc20/transfer.go
+++ erc20/transfer.go
@@ -1,1 +1,51 @@
 package erc20
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+	"log"
+	"math/big"
+)
+
+type Transfer struct {
+	client          *ethclient.Client
+	contractAddress common.Address
+}
+
+// 初始化主网连接
+func InitClient(url, contractAddress string) *Transfer {
+	rpcDial, err := rpc.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+	client := ethclient.NewClient(rpcDial)
+	return &Transfer{client: client, contractAddress: common.HexToAddress(contractAddress)}
+}
+
+// 外部转账
+func (t *Transfer) OutTransfer(fromAddress common.Address, toAddress string, amount int64) *types.Transaction {
+	//计算nonce值
+	nonce, err := t.client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//1eth =1000000000000000000 wei
+	value := big.NewInt(amount * 1000000000000000000)
+	//一次打包可以使用的gas上限
+	gasLimit := uint64(21000)
+
+	//由于不确定gas费用，取平均值
+	gasPrice, err := t.client.SuggestGasPrice(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data []byte
+	tx := types.NewTransaction(nonce, common.HexToAddress(toAddress), value, gasLimit, gasPrice, data)
+	return tx
+}
--- go.mod
+++ go.mod
@@ -4,15 +4,14 @@ go 1.13
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/ethereum/go-ethereum v1.9.8
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/jinzhu/gorm v1.9.11
 	github.com/jordan-wright/email v0.0.0-20190819015918-041e0cec78b0
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/kylelemons/go-gypsy v0.0.0-20160905020020-08cad365cd28
 	github.com/mssola/user_agent v0.5.0
 	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
