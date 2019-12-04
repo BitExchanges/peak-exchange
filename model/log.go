882550972e3e9fb4387ce0e12712535631e1d0aa
wangbinzero <wangbbhtt@gmail.com>
2019-12-04 13:47:56 +0800

    erc20转账

--- model/log.go
+++ model/log.go
@@ -3,9 +3,8 @@ package model
 import "time"
 
 type Log struct {
-	Id     int    `json:"id"`      //主键
-	UserId int    `json:"user_id"` //用户ID
-	Event  string `json:"event"`   //事件   登录/退出/注册/交易/充值
-
+	Id      int       `json:"id"`       //主键
+	UserId  int       `json:"user_id"`  //用户ID
+	Event   string    `json:"event"`    //事件   登录/退出/注册/交易/充值
 	CreatAt time.Time `json:"creat_at"` //创建时间
 }
